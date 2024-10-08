4322f9d2dd77647ac250e00e8d053f041e837f58
bego <bego.jukic@gmx.at>
2024-10-08 19:56:19 +0200

    service interaktor changes

--- app/src/main/java/com/example/meetingnotification/ui/Services/SmsSendingServiceInteractor.kt
+++ app/src/main/java/com/example/meetingnotification/ui/Services/SmsSendingServiceInteractor.kt
@@ -3,5 +3,5 @@ package com.example.meetingnotification.ui.services
 import com.example.meetingnotification.ui.contact.ContactReadyForSms
 
 interface SmsSendingServiceInteractor {
-    fun performServiceAction(action : ServiceAction, vararg contacts: List<ContactReadyForSms>)
+    fun performServiceAction(action : ServiceAction,contacts: List<ContactReadyForSms>)
 }
